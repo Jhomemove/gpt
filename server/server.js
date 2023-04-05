c7a2914a55648e00ba6caace116a4c499babff2d
James Freestone <james@homemove.com>
2023-04-05 15:15:59 +0100

    GPT 3.5

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config()
+dotenv.config();
 
-console.log(process.env.OPENAI_API_KEY)
+console.log(process.env.OPENAI_API_KEY);
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,45 +13,41 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from Homer!'
-  })
-})
+    message: 'Hello from Homer!',
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
-    const { Configuration, OpenAIApi } = require("openai");
+    const messages = [
+      { role: 'system', content: 'You are a helpful AI home move advisor.' },
+      { role: 'user', content: 'Hello, who are you?' },
+      { role: 'assistant', content: 'I am Homer, created by Homemove. How can I help you today?' },
+      { role: 'user', content: 'What is your purpose?' },
+      { role: 'assistant', content: 'My purpose is to provide help and advice on making the home moving process easier. I can provide expert tips, advice, and resources on topics related to packing, budgeting, staging, and much more.' },
+      ];
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+      const response = await openai.ChatCompletion.create({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+        temperature: 1,
+        max_tokens: 500,
+      });
+      
+      res.status(200).send({
+        bot: response.choices[0].message.content.trim(),
+      });
 
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "The following is a conversation with a AI homemove advisor. The Advisor is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am Homer created by Homemove. How can I help you today?\nHuman: What is your purpose?  My purpose is to provide help and advice on making the home moving process easier. I can provide expert tips, advice, and resources on topics related to packing, budgeting, staging, and much more.",
-  temperature: 1,
-  max_tokens: 500,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-  stop: ["\n"],
-});
-
-
-    res.status(200).send({
-      bot: response.data.choices[0].text.trim()
-    });
-
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
-
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error || 'Something went wrong');
+      }
+      });
+      
+      app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
