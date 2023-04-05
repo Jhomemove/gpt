8b650e601a6c1c69a8f8b34c699002454380a80c
James Freestone <james@homemove.com>
2023-04-05 13:38:09 +0100

    fine tune

--- server/server.js
+++ server/server.js
@@ -25,18 +25,24 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const { Configuration, OpenAIApi } = require("openai");
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `I am a Homemove advisor called Homer. If you ask me a question that is rooted in Homemove advice, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "this requires expert advice, our homemove advisor will be in touch". ${prompt}`,
-      temperature: 0,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: ["\n"],
-    });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "I am a Homemove advisor called Homer. If you ask me a question that is rooted in Homemove advice, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"this requires expert advice, our homemove advisor will be in touch\".",
+  temperature: 1,
+  max_tokens: 500,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  stop: ["\n"],
+});
+
 
     res.status(200).send({
       bot: response.data.choices[0].text.trim()
