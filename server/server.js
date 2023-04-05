1d12a31dd8a14108ccbcd5985339237dd633667c
James Freestone <james@homemove.com>
2023-04-05 12:35:24 +0100

    Fine tune

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
-console.log(process.env.OPENAI_API_KEY);
+console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,40 +13,45 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from Homer!'
-  });
-});
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    const initialPrompt = "I am a Homemove advisor called Homer. If you ask me a question that is rooted in Homemove advice, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"this requires expert advice, our homemove advisor will be in touch\".";
-    const fullPrompt = initialPrompt + "\n\nQ: " + prompt + "\nA:";
+    const { Configuration, OpenAIApi } = require("openai");
 
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: fullPrompt,
+      prompt: "I am a Homemove advisor called Homer. If you ask me a question that is rooted in Homemove advice, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"this requires expert advice, speak to our homemove advisor hello@homemove.com.\".",
       temperature: 0,
       max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
-      stop: ["\n"]
+      stop: ["\n"],
     });
 
     res.status(200).send({
       bot: response.data.choices[0].text
     });
+
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
